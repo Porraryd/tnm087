681db5fe4a743ac677a72c63a5c99532d8d85911
Porraryd <pontusorraryd@gmail.com>
2014-12-05 10:12:53 +0100

    Part 3

---
+++ Lab3/part1.m
@@ -0,0 +1,12 @@
+imH = imread('HalfHolga.jpg');
+imH = rgb2gray(double(imH)/255);
+
+imS = imread('HalfSony.jpg');
+imS = rgb2gray(double(imS)/255);
+
+imC = imread('HalfCanon.jpg');
+imC = rgb2gray(double(imC)/255);
+
+imSc = imread('HalfScanner.jpg');
+imSc = rgb2gray(double(imSc)/255);
+
